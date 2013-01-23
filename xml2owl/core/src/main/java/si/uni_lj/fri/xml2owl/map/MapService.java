a7d3733a9edf855a8c1a1e1b7502f6c7dfe3298b
Cyprian Laskowski <cyplas@gmail.com>
2013-01-23 12:53:02 +0100

    removed RulesServiceProxy intermediary

--- xml2owl/core/src/main/java/si/uni_lj/fri/xml2owl/map/MapService.java
+++ xml2owl/core/src/main/java/si/uni_lj/fri/xml2owl/map/MapService.java
@@ -9,7 +9,7 @@ import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLException;
 
 import si.uni_lj.fri.xml2owl.util.*;
-import si.uni_lj.fri.xml2owl.rules.Xml2OwlRulesException;
+import si.uni_lj.fri.xml2owl.rules.*;
 
 /** The XML2OWL mapping service. */
 public class MapService {
@@ -18,8 +18,13 @@ public class MapService {
      * together with the input XML to update the OWL provided. */
     public MapResponse map(MapRequest request) throws Xml2OwlRulesException, Xml2OwlMapException, SaxonApiException, OWLException {
 
-        RulesServiceProxy rulesServiceProxy = new RulesServiceProxy();
-        rulesServiceProxy.validateRules(request.getRules());
+        // Rules validation, using RulesService. 
+        RulesService rulesService = new RulesService();
+        ValidationRequest validationRequest = new ValidationRequest();
+        validationRequest.setRules(request.getRules());
+        ValidationResponse validationResponse = rulesService.validateRules(validationRequest);
+
+        // Mapping.
 	Processor xmlProcessor = new Processor(false);
         OWLOntologyManager owlManager = OWLManager.createOWLOntologyManager();
         XmlStringConvertor xmlConvertor = new XmlStringConvertor(xmlProcessor);
