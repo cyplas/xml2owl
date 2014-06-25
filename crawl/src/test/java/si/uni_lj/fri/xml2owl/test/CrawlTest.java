1859eec585c9e84a8cfc378d2fe19ebcb3f5891b
Cyprian Laskowski <cyplas@gmail.com>
2014-06-25 10:26:31 +0200

    Moved webharvest test resource files out from core to crawl projects

--- crawl/src/test/java/si/uni_lj/fri/xml2owl/test/CrawlTest.java
+++ crawl/src/test/java/si/uni_lj/fri/xml2owl/test/CrawlTest.java
@@ -12,7 +12,7 @@ public class CrawlTest {
 
     private void testOnBookSource(String source) throws Exception {
         System.out.println("Running crawlApplication.testBooks(" + source + ") ...");
-        application.run("../core/src/test/resources/data/books/" + source, "webharvest.xml", "downloads", "../owl.xml","rules.xml",true,true);
+        application.run("src/test/resources/data/books/" + source, "webharvest.xml", "downloads", "../owl.xml","rules.xml",true,true);
     }
 
 }
